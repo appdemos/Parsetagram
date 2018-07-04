d932a2286b12bbd981b616f7ad32f33d54e252e3
Dhriti Chawla <chawla5@purdue.edu>
2018-07-04 01:04:05 -0700

    edit

--- app/src/main/java/com/codepath/parsetagram/FeedFragment.java
+++ app/src/main/java/com/codepath/parsetagram/FeedFragment.java
@@ -1,21 +1,54 @@
 package com.codepath.parsetagram;
 
 import android.content.Context;
+import android.graphics.Movie;
 import android.net.Uri;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.codepath.parsetagram.model.Post;
+
+import java.util.ArrayList;
+
 
 public class FeedFragment extends Fragment {
+
+    ArrayList<Post> posts;
+    RecyclerView rvPost;
+    PostAdapter adapter;
+    AsyncHttpClient client;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        client = new AsyncHttpClient();
+        adapter = new PostAdapter(posts);
+        posts = new ArrayList<>();
+        rvPost = (RecyclerView) rootView.findViewById(R.id.rvPosts);
+
+        rvPost.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvPost.setAdapter(adapter);
+
+        getConfiguration();
+
+        return rootView;
+
     }
 
+
+    public void getConfiguration() {
+
+    }
 }
---
+++ app/src/main/java/com/codepath/parsetagram/PostAdapter.java
@@ -0,0 +1,72 @@
+package com.codepath.parsetagram;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.parsetagram.model.Post;
+
+import java.util.List;
+
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    private List<Post> mPosts;
+    Context context;
+    //pass in the tweets array
+    public PostAdapter(List<Post> posts) {
+        mPosts = posts;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_post,parent,false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        Post post = mPosts.get(position);
+        holder.tvUserName.setText(post.user.name); //????
+        holder.tvDesc.setText(post.description); //????
+
+        Glide.with(context).load(post.user.profileImageUrl).into(holder.ivImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView ivImage;
+        public TextView tvUserName;
+        public TextView tvDesc;
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            ivImage = (ImageView) itemView.findViewById(R.id.ivImage);
+            tvUserName = (TextView) itemView.findViewById(R.id.tvUser);
+            tvDesc = (TextView) itemView.findViewById(R.id.tvDescription);
+
+        }
+    }
+}
